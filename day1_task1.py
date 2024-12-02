faa27182769bc82e74a26258a65a47efcfb29374
noraben <noora.benei@gmail.com>
2024-12-02 23:12:37 +0100

    this reads the data incorrectly from .csv

--- day1_task1.py
+++ day1_task1.py
@@ -1,9 +1,22 @@
 import numpy as np
 
-location_list = np.genfromtxt('day1_task1.csv', delimiter=',')
+location_list = np.genfromtxt('day1_task1.csv', delimiter=',', dtype=int)
 
-first_list = location_list.[:, 0]
-second_list = location_list.[:, 1]
+print(location_list[0:10,:])
+
+first_list = location_list[:, 0]
+second_list = location_list[:, 1]
+
+first_list[0] = 80784
+
+print(first_list[0])
+
+first_list.sort()
+second_list.sort()
+
+result = sum(first_list - second_list)
+
+print(result)
 
 
 
