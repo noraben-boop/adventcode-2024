b2eb880a4b6ff331779e51ed1cf666f215eed82d
noraben <noora.benei@gmail.com>
2024-12-03 09:51:43 +0100

    Second part success - working with lists

--- day1_task1.py
+++ day1_task1.py
@@ -2,14 +2,27 @@ import numpy as np
 
 location_list = np.genfromtxt("input.txt", delimiter='', dtype=int)
 
-print(location_list[0:10,:])
-
 first_list = location_list[:, 0]
 second_list = location_list[:, 1]
 
 first_list.sort()
 second_list.sort()
 
-result = sum(abs(first_list - second_list))
+# part 1
 
-print(result)
+result1 = sum(abs(first_list - second_list))
+
+print(f"part 1 result: {result1}")
+
+first_list = first_list.tolist()
+second_list = second_list.tolist()
+
+print(first_list[0:10])
+
+scores = []
+
+for i in range(len(first_list)):
+    score = first_list[i] * second_list.count(first_list[i])
+    scores.append(score)
+
+print(f"part 2 result: {sum(scores)}")
